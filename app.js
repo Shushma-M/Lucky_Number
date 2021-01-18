da82b7df00c473b300ec30fa1312a54b4338d327
Shushma-M <shushmagowda22@gmail.com>
2021-01-18 23:33:40 +0530

    feat: addded graphics

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ var clck=document.querySelector("#btnClick");
 var cls=document.querySelector("#close");
 var op=document.querySelector("#output");
 var sum=0;
-
+function enableButton(){
+    console.log(ip1.value);
+    console.log(ip2.value);
+    if (ip1.value != "" && ip2.value != "") {
+        clck.disabled = false;
+    } else {
+        clck.disabled = true;
+    }
+}
 function eventHandler(){
+    // document.querySelectorAll("animate").forEach(element => {
+    //     element.beginElement();});
     var inp1=ip1.value;
     var inp2=ip2.value;
     var temp1=inp1.replace(/-/g,"");
@@ -21,16 +31,26 @@ function eventHandler(){
     sum=sum+temp2;
     var res=sum%inp2;
     if(res === 0){
-        var opmsg="You are right, That's your Lucky Number"
+        var opmsg="That's your Lucky Number";
+        document.getElementById("ball").style.fill = "green";
+        op.style.color= "#006400";
+        op.style.backgroundColor= "rgba(124,252,0, 0.5)";
     }
     else{
-        var opmsg="Oops!, That's not your Lucky Number"
+        var opmsg="That's not your Lucky Number";
+        document.getElementById("ball").style.fill = "red";
+        op.style.color= "#B22222";
+        op.style.backgroundColor= "rgba(240,128,128, 0.5)";
     }
-    op.innerText=""+opmsg;
+    op.innerHTML = opmsg;
     sum=0;
+    
+   
 }
 function closeAction(){
     document.getElementById("notice").style.display = 'none';
 }
 cls.addEventListener("click",closeAction);
+ip1.addEventListener("change", enableButton);
+ip2.addEventListener("change", enableButton);
 clck.addEventListener("click",eventHandler);
